1905bedacfea61080953c2c4590e4a95279835cf
Johannes Schmitt <johannes.schmitt@live.com>
2015-10-19 16:03:35 +0200

    use NuGet 3 to create packages; use replacement tokens

--- DaVinci/DaVinci/Properties/AssemblyInfo.cs
+++ DaVinci/DaVinci/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DaVinci")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DaVinci code analyzer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("DaVinci")]
 [assembly: AssemblyTrademark("")]
