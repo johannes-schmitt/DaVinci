1ea616f4734ee792111c0d8ababf6379f218ad68
Johannes Schmitt <johannes.schmitt@live.com>
2015-10-20 16:44:50 +0200

    implemented rule #2: don't use the else keyword

---
+++ Source/DaVinci.UnitTest/ObjectCalisthenics/DoNotUseTheElseKeywordTest.cs
@@ -0,0 +1,114 @@
+﻿using DaVinci.Test.Helpers;
+using DaVinci.Test.Verifiers;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaVinci.Test.ObjectCalisthenics
+{
+    [TestClass]
+    public class DoNotUseTheElseKeywordTest : DiagnosticVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DaVinci.ObjectCalisthenics.DoNotUseTheElseKeyword();
+        }
+
+        [TestMethod]
+        public void MethodContainsIfWithoutElse_NoDiagnosticReported()
+        {
+            const string Code = @"
+            class SomeClass
+            {
+                public void Do()
+                {
+                    if (true)
+                    {
+                        System.Console.WriteLine();
+                    }
+                }
+            }";
+
+            VerifyCSharpDiagnostic(Code);
+        }
+
+        [TestMethod]
+        public void MethodContainsIfWithElse_OneDiagnosticReported()
+        {
+            const string Code = @"
+            class SomeClass
+            {
+                private int firstField;
+
+                private int secondField;
+
+                public void Do()
+                {
+                    if (true)
+                    {
+                        System.Console.WriteLine();
+                    }
+                    else
+                    {
+                        System.Console.WriteLine();
+                    }
+                }
+            }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.2",
+                Message = "The else keyword should be avoided.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 14, 21) }
+            };
+            VerifyCSharpDiagnostic(Code, expected);
+        }
+
+        [TestMethod]
+        public void MethodContainsIfWithTwoElses_TwoDiagnosticReported()
+        {
+            const string Code = @"
+            class SomeClass
+            {
+                private int firstField;
+
+                private int secondField;
+
+                public void Do()
+                {
+                    if (true)
+                    {
+                        System.Console.WriteLine();
+                    }
+                    else if (false)
+                    {
+                        System.Console.WriteLine();
+                    }
+                    else
+                    {
+                        System.Console.WriteLine();
+                    }
+                }
+            }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.2",
+                Message = "The else keyword should be avoided.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 14, 21) }
+            };
+
+            var expected2 = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.2",
+                Message = "The else keyword should be avoided.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 18, 21) }
+            };
+            VerifyCSharpDiagnostic(Code, expected, expected2);
+        }
+    }
+}
---
+++ Source/DaVinci/ObjectCalisthenics/DoNotUseTheElseKeyword.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace DaVinci.ObjectCalisthenics
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DoNotUseTheElseKeyword : BaseDiagnosticAnalyzer
+    {
+        protected override string DiagnosticId => "DaVinci.OC.2";
+        protected override LocalizableString Title => "Don’t use the else keyword (Object Calisthenics Rule #2)";
+        protected override LocalizableString MessageFormat => "The else keyword should be avoided.";
+        protected override LocalizableString Description => "Rule #2 of Object Calisthenics is \"Don’t use the else keyword\".";
+        protected override string Category => "Object Calisthenics";
+        protected override DiagnosticSeverity DefaultSeverity => DiagnosticSeverity.Warning;
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCodeBlockAction(AnalyzeMethodCodeBlock);
+        }
+
+        private void AnalyzeMethodCodeBlock(CodeBlockAnalysisContext context)
+        {
+            foreach (var elseClause in context.CodeBlock.DescendantNodes().OfType<ElseClauseSyntax>())
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, elseClause.GetLocation()));
+            }
+        }
+    }
+}
