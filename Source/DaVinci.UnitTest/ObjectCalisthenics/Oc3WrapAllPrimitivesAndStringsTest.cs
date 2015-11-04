907bf10e2f8f77e1cb01f70eaa5a76096918c79f
Johannes Schmitt <johannes.schmitt@live.com>
2015-11-04 14:58:06 +0100

    basic implementation of OC3

--- Source/DaVinci.UnitTest/ObjectCalisthenics/Oc3WrapAllPrimitivesAndStringsTest.cs
+++ Source/DaVinci.UnitTest/ObjectCalisthenics/Oc3WrapAllPrimitivesAndStringsTest.cs
@@ -2,6 +2,7 @@
 using DaVinci.Test.Helpers;
 using DaVinci.Test.Verifiers;
 
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -20,6 +21,104 @@ namespace DaVinci.Test.ObjectCalisthenics
         {
             var analyzer = GetCSharpDiagnosticAnalyzer();
             analyzer.VerifyAllHelpLinks();
+        }
+
+        [TestMethod]
+        public void MethodTakesIntAsParameter_DiagnosticReported()
+        {
+            const string Code = @"
+                class SomeClass
+                {
+                    public void DoSomething(int parameter)
+                    {
+                    }
+                }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.3",
+                Message = "'parameter' should be wrapped as it's a primitive.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 4, 49) }
+            };
+
+            VerifyCSharpDiagnostic(Code, expected);
+        }
+
+        [TestMethod]
+        public void MethodTakesUIntAsParameter_DiagnosticReported()
+        {
+            const string Code = @"
+                class SomeClass
+                {
+                    public void DoSomething(uint parameter)
+                    {
+                    }
+                }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.3",
+                Message = "'parameter' should be wrapped as it's a primitive.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 4, 50) }
+            };
+
+            VerifyCSharpDiagnostic(Code, expected);
+        }
+
+        [TestMethod]
+        public void MethodTakesNonPrimitiveAsParameter_NoDiagnosticReported()
+        {
+            const string Code = @"
+                class NonPrimitive
+                {
+                }
+
+                class SomeClass
+                {
+                    public void DoSomething(NonPrimitive parameter)
+                    {
+                    }
+                }";
+
+            VerifyCSharpDiagnostic(Code);
+        }
+
+        [TestMethod]
+        public void MethodTakesStringAsParameter_DiagnosticReported()
+        {
+            const string Code = @"
+                class SomeClass
+                {
+                    public void DoSomething(string parameter)
+                    {
+                    }
+                }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DaVinci.OC.3",
+                Message = "'parameter' should be wrapped as it's a primitive.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 4, 52) }
+            };
+
+            VerifyCSharpDiagnostic(Code, expected);
+        }
+
+        [TestMethod]
+        public void MethodTakesObjectAsParameter_DiagnosticReported()
+        {
+            const string Code = @"
+                class SomeClass
+                {
+                    public void DoSomething(object parameter)
+                    {
+                    }
+                }";
+
+            VerifyCSharpDiagnostic(Code);
         }
     }
 }
--- Source/DaVinci/ObjectCalisthenics/Oc3WrapAllPrimitivesAndStrings.cs
+++ Source/DaVinci/ObjectCalisthenics/Oc3WrapAllPrimitivesAndStrings.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace DaVinci.ObjectCalisthenics
@@ -8,13 +10,25 @@ namespace DaVinci.ObjectCalisthenics
     {
         protected override string DiagnosticId => "DaVinci.OC.3";
         protected override LocalizableString Title => "Wrap all primitives and strings (Object Calisthenics Rule #3).";
-        protected override LocalizableString MessageFormat => "";
+        protected override LocalizableString MessageFormat => "'{0}' should be wrapped as it's a primitive.";
         protected override LocalizableString Description => "Rule #3 of Object Calisthenics is \"Wrap all primitives and strings\".";
         protected override string Category => "Object Calisthenics";
         protected override DiagnosticSeverity DefaultSeverity => DiagnosticSeverity.Info;
 
         public override void Initialize(AnalysisContext context)
         {
+            context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.Parameter);
+        }
+
+        private void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var parameterSyntax = context.Node as ParameterSyntax;
+
+            var predefinedTypeSyntax = parameterSyntax?.Type as PredefinedTypeSyntax;
+            if (predefinedTypeSyntax != null && predefinedTypeSyntax.Keyword.Text != "object")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, parameterSyntax.Identifier.GetLocation(), parameterSyntax.Identifier.Text));
+            }
         }
     }
 }
